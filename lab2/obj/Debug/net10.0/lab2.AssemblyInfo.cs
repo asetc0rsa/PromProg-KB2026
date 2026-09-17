4511e6037dbdaf7b1d69a6166d83d13d69c9470c
Lukashevich_Artem <lukashevich_aj_25@student.grsu.by>
2026-09-18 00:42:26 +0300

    Added other configuration files from Google Drive

--- lab2/obj/Debug/net10.0/lab2.AssemblyInfo.cs
+++ lab2/obj/Debug/net10.0/lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dabadf9a5c5a2a1e3feb4b936265f6701ce3c6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992401cf4cb123ff4f39be521eebbe27e8325c23")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
