f93ef0865d8b3932a4a0f1d00c9a42cda67c2c1c
Lukashevich_Artem <lukashevich_aj_25@student.grsu.by>
2026-09-17 23:07:46 +0300

    Added lab2 folder: implement Player class with circular movement logic

---
+++ lab2/Player.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Player
+{
+    public string name;
+    public int location;
+    public State state = State.NotInGame;
+    public int distanceTraveled = 0;
+
+    public Player(string name)
+    {
+        this.name = name;
+        this.location = -1;
+    }
+
+    public void Move(int steps, int fieldSize)
+    {
+        if (state == State.NotInGame)
+        {
+            location = steps;
+            state = State.Playing;
+        }
+        else
+        {
+            distanceTraveled += Math.Abs(steps);
+            
+            int idx = location - 1;
+            idx = (idx + steps) % fieldSize;
+            
+            if (idx < 0) 
+            {
+                idx += fieldSize;
+            }
+            
+            location = idx + 1;
+        }
+    }
+}
